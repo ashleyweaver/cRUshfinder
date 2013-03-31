dfe314c5843ac08e73e267876c48ee345f7a9621
Daniel Bordak <dbordak@gmail.com>
2013-03-31 05:33:46 -0400

    Merge actually merges now

--- merge.py
+++ merge.py
@@ -9,16 +9,25 @@ f = open("ru_crushes-user_timeline.json","r")
 nf = open("cache.json","a")
 fcheck = open("curmax.txt","r")
 
-curMax = fcheck.readline().strip()
+curMax = int(fcheck.readline().strip())
+fcheck.close()
 
 uncleanList = json.load(f)
 f.close()
 
+trimmedList = []
+
 i = 0
 while i<200:
+	if int(uncleanList[199-i]['id']) > curMax:
+		trimmedList.insert(0,uncleanList[199-i])
+		curMax = int(uncleanList[199-i]['id'])
 	i+=1
 
+fwrite = open("curmax.txt","w")
+fwrite.write(str(curMax) + "\n")
+fwrite.close()
 
-cleanedList = strip.main(uncleanList)
+cleanedList = strip.main(trimmedList)
 json.dump(cleanedList,nf)
 nf.close()
