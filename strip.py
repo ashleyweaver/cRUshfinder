293ce6470d4ed485980ea63723b9cf4591a9c671
Daniel Bordak <dbordak@gmail.com>
2013-03-31 11:08:34 -0400

    BRB BREAKING EVERYTHING

--- strip.py
+++ strip.py
@@ -7,7 +7,7 @@ from operator import itemgetter
 # stripped array will then be appended to the cache.
 
 # These are the 100 most commonly used words in English according to Wikipedia.
-removedWords = ["the","be","to","of","and","a","in","that","have","i","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us"]
+removedWords = ["the","be","to","of","and","a","in","that","have","i","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us",""]
 
 def main(uncleanList):
 	cleanedList = []
