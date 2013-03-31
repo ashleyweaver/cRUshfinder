e98142284717fbbe5391e70908597a5a725755e5
Daniel Bordak <dbordak@gmail.com>
2013-03-31 08:46:27 -0400

    Added full match parameter to search

--- search.py
+++ search.py
@@ -2,7 +2,7 @@ import string
 import simplejson as json
 from operator import itemgetter
 
-def main(nam,fromDate):
+def main(nam,fullMatch,fromDate):
 	names = string.split(string.lower(nam))
 	f = open("cache.json","r")
 	postList = json.load(f)
@@ -16,18 +16,25 @@ def main(nam,fromDate):
 				if word == name:
 					item["coincidence"]+=1
 					break
-	
-	# Remove all tweets with 0 coincidence
-	i=0
-	while i<len(postList):
-		if postList[i]["coincidence"]==0:
-			del postList[i]
-		else:
-			i+=1
-	
-	# Sort it ascending because I can't figure out how the hell
-	# to sort it descending
-	postList.sort(key=itemgetter('coincidence')) 
+	if fullMatch:
+		i=0
+		while i<len(postList):
+			if postList[i]["coincidence"]!=len(names):
+				del postList[i]
+			else:
+				i+=1
+	else:
+		# Remove all tweets with 0 coincidence
+		i=0
+		while i<len(postList):
+			if postList[i]["coincidence"]==0:
+				del postList[i]
+			else:
+				i+=1
+
+		# Sort it ascending because I can't figure out how the hell
+		# to sort it descending
+		postList.sort(key=itemgetter('coincidence')) 
 
 	# Then I just append the contents to an empty array, popping
 	# them to reverse the order
